4b4e1748390c3a356f759df09fccdcf9bec35ad0
emeric <marion.emeric@laposte.net>
2020-05-06 15:03:23 +0200

    create articles page

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,18 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticlesController extends AbstractController
 {
     /**
      * @Route("/articles", name="articles")
+     * @param ProductRepository $products
+     * @return Response
      */
-    public function index()
+    public function index(ProductRepository $products)
     {
+        $products = $products->findAll();
+
         return $this->render('articles/index.html.twig', [
-            'controller_name' => 'ArticlesController',
+            'products' => $products,
         ]);
     }
 }
