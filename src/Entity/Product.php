0499303371ae2142bac2eca9441f05d9e49fc22b
emeric <marion.emeric@laposte.net>
2020-05-06 22:45:25 +0200

    create articles page

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,10 +2,15 @@
 
 namespace App\Entity;
 
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ * @ORM\Table(name="product")
+ * @Vich\Uploadable
  */
 class Product
 {
@@ -17,23 +22,47 @@ class Product
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $name;
 
     /**
-     * @ORM\Column(type="text")
+     * @ORM\Column(type="text", nullable=true)
      */
     private $description;
 
     /**
-     * @ORM\Column(type="text")
+     * @Vich\UploadableField(mapping="product_images", fileNameProperty="name")
+     * @var File
      */
-    private $imageName;
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var DateTime
+     */
+    private $updatedAt;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
     }
 
     public function getName(): ?string
@@ -60,14 +89,32 @@ class Product
         return $this;
     }
 
-    public function getImageName(): ?string
+    public function setImageFile(File $name = null)
     {
-        return $this->imageName;
+        $this->imageFile = $name;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($name) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
     }
 
-    public function setImageName(string $imageName): self
+    public function getImageFile()
     {
-        $this->imageName = $imageName;
+        return $this->imageFile;
+    }
+
+    public function getUpdatedAt(): ?DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
