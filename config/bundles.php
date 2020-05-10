a9d7d667a321dd2014afb8674577c96de655e38e
emeric <marion.emeric@laposte.net>
2020-05-10 12:16:39 +0200

    reset password

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
