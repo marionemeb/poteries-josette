6e1038097386dd95c32b2088a3e24f9750185059
emeric <marion.emeric@laposte.net>
2020-05-12 16:25:06 +0200

    add print button for Recipes

---
+++ src/Controller/PdfController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recipe;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PdfController extends AbstractController
+{
+    /**
+     * @Route("/pdf/{id}", name="pdf")
+     * @param $id
+     */
+    public function generate_pdf($id){
+        $recipe = new Recipe();
+        $recipe = $this->getDoctrine()->getRepository(Recipe::class)->find($id);
+
+        $options = new Options();
+        $options->set('defaultFont', 'Roboto');
+
+        $dompdf = new Dompdf($options);
+
+        $data = array(
+            'headline' => 'my headline'
+        );
+        $html = $this->renderView('pdf/index.html.twig', [
+            'recipe' => $recipe
+        ]);
+
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->stream("recette_josette.pdf", [
+            "Attachment" => true
+        ]);
+    }
+}
